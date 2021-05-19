7e2132923d5a67e5f9a685842225ff352735a8dd
gnurtel <lehoangtrung038582@gmail.com>
2021-05-19 22:16:37 +0700

    first commit

---
+++ test03/trung.java
@@ -0,0 +1,17 @@
+package test03;
+public class trung {
+    public static int sumOfArray(int a[]){
+        int sum = 0;
+        for(int i = 0 ; i < a.length; i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+    public static double sumOfArray(double a[]){
+        double sum = 0;
+        for(int i = 0; i < a.length; i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+}
